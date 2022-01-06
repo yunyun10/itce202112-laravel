e012f23d185e6772ea14ccb8a78d09ff8382c243
Ayuna KATORI <sudaxoxo@gmail.com>
2022-01-06 16:10:15 +0900

    nav bar作成のblade,controller,route準備

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+   function index()
+   {
+       return view('tasks');
+   }
 }
---
+++ backend/resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <title>ToDoAPP</title>
+<body>
+<header>Navbar will be here</header>
+<div>
+    @yield('content')
+</div>
+<footer>copy rights by ToDoApp</footer>
+
+
+</body>
+</head>
+
+
+
+
+
+
+
+
+
+
+</html>
