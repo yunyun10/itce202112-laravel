1cc43151506dc431c07a9553ee94856c2bc25ea2
Ayuna KATORI <sudaxoxo@gmail.com>
2022-01-19 13:47:47 +0900

    nav-barをトップに固定

--- backend/resources/views/layout.blade.php
+++ backend/resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>ToDoAPP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">ToDoAPP</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
