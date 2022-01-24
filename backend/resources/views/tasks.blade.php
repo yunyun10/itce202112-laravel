c81a36acc8195c50eca68468b9cebfbf65eabda6
Ayuna KATORI <sudaxoxo@gmail.com>
2022-01-24 16:00:20 +0900

    変更

--- backend/resources/views/tasks.blade.php
+++ backend/resources/views/tasks.blade.php
@@ -40,7 +40,7 @@
                             <a class="dropdown-item" href="/">完了したタスク</a>
                         </div>
                     </td>
-                    <td width="100"><button type="button" class="btn .border.border-0 w-auto dropdown-toggle btn-sm">
+                    
                 </div>
             </tr>
             </div>
