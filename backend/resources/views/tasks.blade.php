bec6a8012248eb0c4d200ece428b71f0078bf3b5
Ayuna KATORI <sudaxoxo@gmail.com>
2022-01-24 13:33:05 +0900

    新規作成の位置変更

--- backend/resources/views/tasks.blade.php
+++ backend/resources/views/tasks.blade.php
@@ -7,61 +7,107 @@
     <title>Basic Tasks</title>
     <!-- <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
     @extends('layout')
     @section('content')
     @stop
 </head>
 
 <body>
-    <div class="card mb-3">
-        <div class="card-header">タスク新規追加</div>
-        <div class="card-body">
-            <form method="POST" action="/create">
-                @csrf
-                <div class="form-group">
-                    <input type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="タスク名">
-                    @if ($errors->has('name'))
-                        <p class="text-danger">{{ $errors->first('name') }}</p>
-                    @endif
-                    <input type="datetime-local" name="deadline" class="form-control w-25" placeholder="締め切り日">
-                    @if ($errors->has('deadline'))
-                        <p class="text-danger">{{ $errors->first('deadline') }}</p>
-                    @endif
-                    <div class="text-end">
-                        <button type="submit" class="btn btn-outline-dark"><i class="fas fa-plus fa-lg mr-2"
-                                style="color: grey;"></i>追加</button>
-                    </div>
+    <h1>
+        <table>
+            <tr>
+                <div class="btn-group" role="group" aria-label="ボタングループサンプル">
+                    <td><button type="button" class="btn .border.border-0 w-auto dropdown-toggle btn-sm"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                                class="fa fa-arrow-up"></i><i class="fa fa-arrow-down">ソート</i></button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item " href="/">@sortablelink('deadline', '期日並び替え')</a>
+                            <a class="dropdown-item" href="/">@sortablelink('created_at', '作成日並び替え')</a>
+                        </div>
+                    </td>
+                    <td><button type="button" class="btn .border.border-0 w-auto dropdown-toggle btn-sm"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                                class="fa fa-arrow-up"></i><i class="fa fa-arrow-down">タスク一覧</i></button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/">全てのタスク</a>
+                            <a class="dropdown-item" href="/">未完了のタスク</a>
+                            <a class="dropdown-item" href="/">完了したタスク</a>
+                        </div>
+                    </td>
+                    <style>
+                        .btn-group {
+                            position: relative;
+                            top: 600px;
+                        }
+
+                    </style>
                 </div>
-            </form>
-        </div>
+            </tr>
+            </div>
+        </table>
+    </h1>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    </div>
 </body>
 <div class="card">
     <div class="card-header">タスク一覧</div>
     <div class="card-body">
         @if (count($tasks) > 0)
-            <table class="table table-striped">
+            <table class="table table-striped table-bordered">
                 <tbody>
-                    <thead>
-                        <tr>
+                    <tr>
+                        <td>新規作成</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
 
-
-                            <td></td>
-                            <td>@sortablelink('deadline', '並び替え')</td>
-                            <td>@sortablelink('created_at', '並び替え')</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-
-                        </tr>
+                    </tr>
                     </thead>
                     <tr>
-                        <th>タスク名</th>
-                        <th>期日</th>
-                        <th>作成日</th>
-                        <th></th>
+                        <form method="POST" action="/create">
+                            @csrf
+                            <th>
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" value="{{ old('name') }}"
+                                        placeholder="タスク名">
+                                    @if ($errors->has('name'))
+                                        <p class="text-danger">{{ $errors->first('name') }}</p>
+                                    @endif
+                            </th>
+                            <th><input type="datetime-local" name="deadline" class="form-control" placeholder="締め切り日">
+                                @if ($errors->has('deadline'))
+                                    <p class="text-danger">{{ $errors->first('deadline') }}</p>
+                                @endif
+                            </th>
+                            <th>
+                                <div class="text-start">
+                                    <button type="submit" class="btn btn-outline-dark align-items-center"><i
+                                            class="fas fa-plus fa-lg mr-2" style="color: grey;"></i>タスクを追加</button>
+                            </th>
+                            <th></th>
+                        </form>
                         <th></th>
                         <th></th>
                     </tr>
+
                     @foreach ($tasks as $task)
                         @php
                             $today = date('Y-m-d');
@@ -103,13 +149,13 @@
                             </td>
                         </tr>
                     @endforeach
-
                 </tbody>
             </table>
         @endif
     </div>
 </div>
 </div>
+
 </body>
 
 </html>
